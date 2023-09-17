5cba36c6acbddbcbd021ef2113cdf2d7868cd12c
Erick Silva <erick.fonseca916@gmail.com>
2023-09-17 17:52:27 -0300

    arquivo adicionado

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Paragraph from './components/transform/paragraph';
+import Button from './components/button/button';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Paragraph paragraph="Hello dev's! Esse projeto é um desafio do curso DevQuest" />
+      <Button label='Clique aqui :)' />
     </div>
   );
 }
---
+++ src/components/transform/paragraph.js
@@ -0,0 +1,7 @@
+import './paragraph.css'
+
+const Paragraph = ({paragraph}) => {
+    return <p className="paragrafo">{paragraph}</p>
+}
+
+export default Paragraph
